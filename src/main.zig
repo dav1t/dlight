dababc2a856966407c3cc31be5472b8876598803
Davit Mamulashvili <dav1t.mam@gmail.com>
2026-02-21 11:57:09 +0400

    Add basic REPL and .gitignore
    REPL: prompt and stdin loop, meta command handling (.exit), statement
    preparation for "insert" and "select", execution stubs, and flushes
    after
    writes.
    
    .gitignore: patterns for Zig build/cache, build artifacts, debug files,
    editor/IDE files, and ZLS cache.

--- src/main.zig
+++ src/main.zig
@@ -3,49 +3,92 @@ const Io = std.Io;
 
 const dlight = @import("dlight");
 
-pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const MetaCommandResult = enum { meta_command_success, meta_command_unrecognized_command };
+const PrepareResult = enum { perpare_success, pepare_unrecognized_statement };
 
-    // This is appropriate for anything that lives as long as the process.
-    const arena: std.mem.Allocator = init.arena.allocator();
+const StatementType = enum { statement_insert, statement_select };
 
-    // Accessing command line arguments:
-    const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
+const Statement = struct { type: StatementType };
+
+fn printPrompt(writer: *Io.Writer) Io.Writer.Error!void {
+    try writer.print("db > ", .{});
+}
+
+fn executeStatement(writer: *Io.Writer, statement: *Statement) !void {
+    switch (statement.type) {
+        .statement_insert => {
+            try writer.print("This is where we would do an insert. \n", .{});
+        },
+        .statement_select => {
+            try writer.print("This is where would do a select \n", .{});
+        },
+    }
+}
+
+fn prepareStatement(input: *const []u8, statement: *Statement) PrepareResult {
+    if (std.mem.eql(u8, input.*, "insert")) {
+        statement.type = StatementType.statement_insert;
+        return PrepareResult.perpare_success;
     }
 
-    // In order to do I/O operations need an `Io` instance.
+    if (std.mem.eql(u8, input.*, "select")) {
+        statement.type = StatementType.statement_select;
+        return PrepareResult.perpare_success;
+    }
+
+    return PrepareResult.pepare_unrecognized_statement;
+}
+
+fn doMetaCommand(input: *const []u8) MetaCommandResult {
+    if (std.mem.eql(u8, input.*, ".exit")) {
+        std.process.exit(0);
+    } else {
+        return MetaCommandResult.meta_command_unrecognized_command;
+    }
+}
+
+pub fn main(init: std.process.Init) !void {
     const io = init.io;
 
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     var stdout_buffer: [1024]u8 = undefined;
+    var stdin_buffer: [1024]u8 = undefined;
+
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
+    const stdout = &stdout_file_writer.interface;
 
-    try dlight.printAnotherMessage(stdout_writer);
+    while (true) {
+        try printPrompt(stdout);
+        try stdout.flush();
 
-    try stdout_writer.flush(); // Don't forget to flush!
-}
+        var stdin_file: Io.File.Reader = .init(.stdin(), io, &stdin_buffer);
+        const stdin = &stdin_file.interface;
+        const line = try stdin.takeDelimiterExclusive('\n');
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+        // If command is meta command
+        if (line[0] == '.') {
+            switch (doMetaCommand(&line)) {
+                MetaCommandResult.meta_command_success => continue,
+                MetaCommandResult.meta_command_unrecognized_command => {
+                    try stdout.print("Unrecognized command '{s}' \n", .{line});
+                    try stdout.flush();
+                    continue;
+                },
+            }
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+
+        var statement: Statement = Statement{ .type = .statement_insert };
+
+        switch (prepareStatement(&line, &statement)) {
+            .pepare_unrecognized_statement => {
+                try stdout.print("Unrecognized keyword at start of '{s}' \n", .{line});
+                try stdout.flush();
+                continue;
+            },
+            .perpare_success => {},
+        }
+
+        try executeStatement(stdout, &statement);
+        try stdout.print("Executed. \n", .{});
+        try stdout.flush();
+    }
 }
